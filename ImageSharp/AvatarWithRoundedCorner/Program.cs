5b1f442c1c4f7bec7c28f1837e314176c5eedf59
Jarno Montonen <jarno.montonen@futurice.com>
2018-08-30 14:05:56 +0300

    Update Program.cs

--- ImageSharp/AvatarWithRoundedCorner/Program.cs
+++ ImageSharp/AvatarWithRoundedCorner/Program.cs
@@ -28,6 +28,7 @@ namespace AvatarWithRoundedCorner
 
                 using (Image<Rgba32> destRound = img.Clone(x => x.ConvertToAvatar(new Size(200, 200), 100)))
                 {
+
                     destRound.Save("output/fb-round.png");
                 }
 
